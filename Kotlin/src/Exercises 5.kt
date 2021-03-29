eefe0aed41b5ab11d08ab288e573e7894dccec1d
donnyrivaldi12 <81557212+donnyrivaldi12@users.noreply.github.com>
2021-03-29 15:39:37 +0700

    Add files via upload

---
+++ Kotlin/src/Exercises 5.kt
@@ -0,0 +1,55 @@
+fun main(){
+    val angka1 = 10.toDouble()
+    val angka2 = 20.toDouble()
+    val angka3 = 30.toDouble()
+    val angka4 = 40.toDouble()
+    val angka5 = 50.toDouble()
+    val angka6 = 60.toDouble()
+    val angka7 = 70.toDouble()
+    val angka8 = 80.toDouble()
+    val angka9 = 90.toDouble()
+    val angka10= 100.toDouble()
+
+    println(angka1)
+    println(angka2)
+    println(angka3)
+    println(angka4)
+    println(angka5)
+    println(angka6)
+    println(angka7)
+    println(angka8)
+    println(angka9)
+    println(angka10)
+    println()
+
+    var kata1 = "Saya "
+    var kata2 = "Sedang "
+    var kata3 = "Belajar "
+    var kata4 = "Bahasa "
+    var kata5 = "Pemrograman "
+    var kata6 = "Kotlin "
+    var kata7 = "Dengan "
+    var kata8 = "Menggunakan "
+    var kata9 = "IntelliJ "
+    var kata10= "IDEA"
+
+    println(kata1)
+    println(kata2)
+    println(kata3)
+    println(kata4)
+    println(kata5)
+    println(kata6)
+    println(kata7)
+    println(kata8)
+    println(kata9)
+    println(kata10)
+    println()
+    println("Hasil Penggabungan Concatenation:")
+    println(kata1+kata2+kata3+kata4+kata5+kata6+kata7+kata8+kata9+kata10)
+    println()
+
+    var umurKu = 20
+    var pesan = "Saya Berumur $umurKu tahun"
+    println(pesan)
+
+}
